be7b2f721381aa5d6a557d5e0d0457c1506ff4ec
Roderick Kennedy <roderick.kennedy@gmail.com>
2023-07-28 17:10:08 +0100

    Added some missing assets.

---
+++ Assets/AnimationTutorial/Scripts/PlayerAnimManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimManager : MonoBehaviour
+{
+    private Animator anim;
+    public NavMeshAgent navMeshAgent;
+    //public bool isWalking;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /*
+        if (navMeshAgent.velocity != Vector3.zero)
+        {
+            isWalking = true;
+        }
+
+        else
+        {
+            isWalking = false;
+        }
+
+        anim.SetBool("IsWalking", isWalking);
+        */
+
+        anim.SetFloat("PlayerVelocity", navMeshAgent.velocity.magnitude);
+
+        //print(navMeshAgent.velocity.magnitude);
+	}
+}
---
+++ Assets/AnimationTutorial/Scripts/TreasureChest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour
+{
+    public bool interactable = false;
+    public bool isOpen = false;
+    private Animator anim;
+
+    public Rigidbody coinPrefab;
+    public Transform spawner;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+          isOpen = !isOpen;
+          anim.SetBool("isOpen", isOpen);
+
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            interactable = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            interactable = false;
+        }
+    }
+
+    public void OpenChest()
+    {
+        Rigidbody coinInstance;
+        coinInstance = Instantiate(coinPrefab, spawner.position, spawner.rotation) as Rigidbody;
+        coinInstance.AddForce(spawner.up * 100);
+    }
+}
